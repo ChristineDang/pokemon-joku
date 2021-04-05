9d82609d4680e6c6f5de5fc86ed772739dc3ffd3
Christine Dang <dangc101@gmail.com>
2021-04-05 14:25:22 -0400

    Basic layout created, py and flask installed

---
+++ pj-app/src/components/cardSelect.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './carousel.css';
+import { Card, Button } from 'react-bootstrap';
+import turtwig from '../images/turtwig.png';
+
+// function card() {
+//     return (
+//         <Card style={{ width: '18rem' }}>
+//         <Card.Img variant="top" src={turtwig} />
+//         <Card.Body>
+//           <Card.Title>Turtwig</Card.Title>
+//           <Card.Text>
+//             Some quick example text to build on the card title and make up the bulk of
+//             the card's content.
+//           </Card.Text>
+//           <Button variant="primary">Select</Button>
+//         </Card.Body>
+//         </Card>
+//     );
+// }
+
+// export default card;
+
+
+
+
